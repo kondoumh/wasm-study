7c66d915d1da42ce731ab0769179d4ae697c0a7c
kondoumh <kondoumh@gmail.com>
2022-09-26 10:23:55 +0900

    first commit

---
+++ wasmtime/gcd-bash/gcd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function gcd() {
+  # Cast to number; default = 0
+  local x=$(($1))
+  local y=$(($2))
+  # Invoke GCD from module; supress stderr
+  local result=$(wasmtime example/gcd.wat --invoke gcd $x $y 2>dev/null)
+  echo "$result"
+}
+
+# main
+for num in "27 6" "6 27" "42 12"; do
+  set -- $num
+  echo "gcd($1, $2) = $(gcd "$1" "$2")"
+done
