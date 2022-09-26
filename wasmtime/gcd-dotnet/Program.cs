742327f3bb07b8e7f5a36840a1a4e1d3d0a8b5bd
kondoumh <kondoumh@gmail.com>
2022-09-26 17:08:43 +0900

    add dotnet example

---
+++ wasmtime/gcd-dotnet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Wasmtime;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var engine = new Engine();
+            using var module = Module.FromTextFile(engine, "gcd.wat");
+
+            using var host = new Host(engine);
+            using dynamic instance = host.Instantiate(module);
+
+            Console.WriteLine($"gcd(27, 6) = {instance.gcd(27, 6)}");
+        }
+    }
+}
